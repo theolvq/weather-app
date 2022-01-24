2b9a2807f9129591df50bdd4cc0a007f0dc589c2
Theo Leveque <theo.leveque@gmail.com>
2022-01-23 22:54:36 -0800

    added homemade logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header({
   response,
 }) {
   return (
-    <header className='flex justify-between items-center max-w-screen-xl pt-8 px-8 mx-auto'>
+    <header className='flex justify-between items-center max-w-screen-xl pt-4 px-8 mx-auto'>
       <Search
         getGeoCodes={getGeoCodes}
         setGeoCodes={setGeoCodes}
@@ -20,8 +20,14 @@ export default function Header({
         setCities={setCities}
         response={response}
       />
-      <h3 className='self-start font-bold text-xl uppercase bg-gradient-to-br from-cyan-300 via-sky-500 to-teal-400 text-transparent bg-clip-text '>
-        Weather App
+      <h3 className='sm:flex hidden self-start  relative font-normal text-xl uppercase tracking-widest group hover:cursor-pointer '>
+        <span className='bg-blue-800 text-slate-50 py-2 pl-4 pr-2 rounded-l-md group group-hover:text-blue-800 group-hover:bg-slate-50  transition-all duration-500'>
+          Your
+        </span>{' '}
+        <span className='text-blue-800 bg-slate-50 py-2 pr-4 pl-2 rounded-r-md group group-hover:text-slate-50 group-hover:bg-blue-800 transition-all duration-500'>
+          Weather
+        </span>
+        <div className='absolute -top-2 -right-2  w-6 h-6 rounded-full bg-gradient-to-tr from-orange-700 group group-hover:-translate-x-52 group-hover:bg-gradient-to-tl transition-all duration-500' />
       </h3>
     </header>
   );
--- components/Search.jsx
+++ components/Search.jsx
@@ -40,25 +40,34 @@ export default function Search({
   }, [city]); //eslint-disable-line
 
   return (
-    <form className='flex items-center gap-4' onSubmit={handleSubmit}>
+    <form
+      className='flex items-center gap-4'
+      autoComplete='off'
+      onSubmit={handleSubmit}
+    >
+      <input
+        autoComplete='false'
+        name='hidden'
+        type='text'
+        className='hidden'
+      />
       <div className='flex flex-col relative z-50'>
         <input
-          className='sm:w-80 w-full text-slate-700 bg-opacity-0 placeholder:text-slate-100 bg-slate-50 border-2 border-blue-700 py-2 px-3 rounded-md  hover:scale-105 focus:bg-opacity-100 focus:border-opacity-0 focus:placeholder:text-slate-700 transition duration-300 ease-in-out'
+          className='sm:w-80 w-full text-slate-100 focus:text-slate-700 bg-opacity-0 placeholder:text-slate-100 bg-slate-50 border-2 border-blue-700 py-2 px-3 rounded-md hover:border-blue-700 hover:bg-slate-50 hover:text-slate-700 focus-visible:outline-blue-700 focus:bg-opacity-100 focus:border-opacity-0 focus:placeholder:text-slate-700 hover:placeholder:text-slate-700 transition duration-300 ease-in-out'
           placeholder='Search for a city'
           type='text'
           name='city'
           value={city}
           onChange={handleChange}
-          autoComplete='off'
         />
 
         {cities.length > 0 && (
-          <ul className='absolute z-40 w-full top-10 pt-2 bg-slate-800  pb-2 rounded-b-md cursor-pointer backdrop-filter backdrop-blur-md'>
+          <ul className='absolute z-40 w-full top-10 pt-2 bg-blue-800  pb-2 rounded-b-md cursor-pointer'>
             {cities.map((city) => (
               <li
                 key={city.lat}
                 value={city.name}
-                className='text-slate-50 hover:bg-blue-700 px-4 py-0.5 rounded-md'
+                className='text-slate-50 hover:bg-blue-500 px-4 py-1 rounded-md'
                 id={city.lat}
                 onClick={handleClick}
               >
@@ -69,7 +78,7 @@ export default function Search({
           </ul>
         )}
       </div>
-      <button className='border-2 border-blue-800 bg-blue-800 text-slate-100 font-semibold rounded-md px-4 py-2 hover:bg-blue-700 transition-all ease-in-out duration-300 hover:scale-105'>
+      <button className='border-2 border-blue-800 bg-blue-800 text-slate-100 font-semibold rounded-md px-4 py-2 hover:bg-blue-700 hover:border-blue-700 transition-all ease-in-out duration-300 hover:scale-105'>
         Search
       </button>
     </form>
